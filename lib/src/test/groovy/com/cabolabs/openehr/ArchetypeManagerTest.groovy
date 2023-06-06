9f94e9208ca44771bdc9bb694bec1cd16bfc88e5
ppazos <pablo.pazos@cabolabs.com>
2023-06-06 19:12:59 -0300

    addded gradle build

--- lib/src/test/groovy/com/cabolabs/openehr/ArchetypeManagerTest.groovy
+++ lib/src/test/groovy/com/cabolabs/openehr/ArchetypeManagerTest.groovy
@@ -1,7 +1,7 @@
 /**
  * junit4 test http://docs.groovy-lang.org/docs/latest/html/documentation/core-testing-guide.html#_junit_4
  */
-package test;
+package com.cabolabs.openehr;
 
 import static org.junit.Assert.*;
 
@@ -21,7 +21,7 @@ class ArchetypeManagerTest {
 
    private static String PS = File.separator
    private static String path = "."+ PS +"resources"+ PS +"archetypes"
-   
+
    /**
     * @throws java.lang.Exception
     */
@@ -74,20 +74,20 @@ class ArchetypeManagerTest {
    {
       def loader = ArchetypeManager.getInstance(path)
       loader.loadAll()
-      
-      
+
+
       def observation_adl_files = new File(path + PS + "entry" + PS + "observation").listFiles().count { it.name ==~ /.*\.adl/ }
-      
+
       assert loader.getArchetypes("composition", ".*").size() == 3 : "No se cargaron todos los arquetipos"
       assert loader.getArchetypes("observation", ".*").size() == observation_adl_files : "No se cargaron todos los arquetipos"
    }
-   
+
    @Test
    public void testGetText()
    {
       def loader = ArchetypeManager.getInstance(path)
       loader.loadAll()
-      
+
       // Todos estos casos de test son correctos
       // los caracteres acentuados se escriben en unicode para evitar problemas de Java en la comparacion
       // https://dtrinf.wordpress.com/2012/02/29/como-escribir-enes-n-y-acentos-en-java/
@@ -101,16 +101,16 @@ class ArchetypeManagerTest {
         ['openEHR-EHR-OBSERVATION.blood_pressure.v1', 'es-ar', 'at0005', 'Di\u00e1stole'],
         ['openEHR-EHR-OBSERVATION.blood_pressure.v1', 'es-ar', 'at0013', 'Tama\u00f1o del manguito']
       ]
-      
+
       def text
       testCases.each { testCase ->
 
          text = loader.getText(testCase[0], testCase[1], testCase[2])
-         
+
          assert text != null
-         
+
          assert text == testCase[3]
-         
+
          println text
       }
    }
@@ -124,35 +124,35 @@ class ArchetypeManagerTest {
    {
       def loader = ArchetypeManager.getInstance(path)
       loader.loadAll()
-      
+
       // Obtiene un arquetipo por id
       def arch = loader.getArchetype("openEHR-EHR-OBSERVATION.blood_pressure.v1")
-      
-      
+
+
       assert arch.archetypeId.value == "openEHR-EHR-OBSERVATION.blood_pressure.v1"
       assert arch.definition.getClass().getSimpleName() == "CComplexObject" // Clase del modelo de arquetipos
-      
-      
-      
+
+
+
       // Muestra todas las paths del arquetipo y a que tipo del modelo de informacion corresponden
       println ""
       arch.physicalPaths().sort().each { path ->
          println path +" ("+ arch.node(path).rmTypeName +")"
       }
       println ""
-      
-      
+
+
       // operaciones sobre nodos del arquetipo
       // restriccion (CDvQuantity) sobre presion sistolica (DvQuantity)
       assert arch.node('/data[at0001]/events[at0006]/data[at0003]/items[at0004]/value').getClass().getSimpleName() == "CDvQuantity"
-      
+
       assert arch.node('/data[at0001]/events[at0006]/data[at0003]/items[at0004]/value').path() == '/data[at0001]/events[at0006]/data[at0003]/items[at0004]/value'
-      
+
       def cdvquantity = arch.node('/data[at0001]/events[at0006]/data[at0003]/items[at0004]/value')
       def constraintList = cdvquantity.getList() // lista de restricciones
-      
+
       assert constraintList.size() == 1 // hay una sola restriccion
-      
+
       def constraint = constraintList[0]
 
       // datos de la restriccion: unidades y rango valido para la magnitud
@@ -160,23 +160,23 @@ class ArchetypeManagerTest {
       assert constraint.magnitude.getClass().getSimpleName() == "Interval"
       assert constraint.magnitude.lower == 0.0d
       assert constraint.magnitude.upper == 1000.0d
-      
-      
-      
+
+
+
       // validacion de un dato clinico contra una restriccion del arquetipo
       def datoUnits = 'mm[Hg]'
       def datoMagnitude = 134.0d
-      
+
       // se podria simplificar porque se sabe que el arquetipo tiene solo una restriccion en la lista
       def valid = constraintList.find { item ->
          // para la restriccion que tiene las mismas unidades que los datos
          // verifica si la magnitud de los datos esta dentro del rango definido en la restriccion
          if (item.units == datoUnits && item.magnitude.has(datoMagnitude)) return true
       } != null
-      
+
       assert valid
-      
-      
+
+
       // diastolica
       //println arch.node('/data[at0001]/events[at0006]/data[at0003]/items[at0005]/value')
    }
@@ -198,14 +198,14 @@ class ArchetypeManagerTest {
    {
       def loader = ArchetypeManager.getInstance(path)
       loader.loadAll()
-      
+
       // Obtiene un arquetipo por id
       def archs = loader.getArchetypes("OBSERVATION", ".*")
-      
+
       def observation_adl_files = new File(path + PS + "entry" + PS + "observation").listFiles().count { it.name ==~ /.*\.adl/ }
-      
+
       assert archs.size() == observation_adl_files
-      
+
       // arquetipos de observacion que hay en /resources
       def existingArchetypes = [
       "openEHR-EHR-OBSERVATION.blood_pressure.v1",
@@ -217,9 +217,9 @@ class ArchetypeManagerTest {
       "openEHR-EHR-OBSERVATION.test_all_datatypes.v1",
       "openEHR-EHR-OBSERVATION.test_servicios_terminologicos.v1"
       ]
-      
+
       archs.each { arch ->
-      
+
          assert existingArchetypes.contains ( arch.archetypeId.value )
       }
    }
@@ -232,16 +232,16 @@ class ArchetypeManagerTest {
    {
       def loader = ArchetypeManager.getInstance(path)
       loader.loadAll()
-      
+
       // en lugar de pedirle un nodo al arquetipo se le puede pedir al ArchetypeManager
       def cdvquantity = loader.getArchetypeNode('openEHR-EHR-OBSERVATION.blood_pressure.v1/data[at0001]/events[at0006]/data[at0003]/items[at0004]/value')
-   
+
       assert cdvquantity.getClass().getSimpleName() == "CDvQuantity"
-   
+
       def constraintList = cdvquantity.getList() // lista de restricciones
-      
+
       assert constraintList.size() == 1 // hay una sola restriccion
-      
+
       def constraint = constraintList[0]
 
       // datos de la restriccion: unidades y rango valido para la magnitud
--- lib/src/test/groovy/com/cabolabs/openehr/ArchetypeWalkthroughTest.groovy
+++ lib/src/test/groovy/com/cabolabs/openehr/ArchetypeWalkthroughTest.groovy
@@ -1,7 +1,7 @@
 /**
  * junit4 test http://docs.groovy-lang.org/docs/latest/html/documentation/core-testing-guide.html#_junit_4
  */
-package test;
+package com.cabolabs.openehr;
 
 import static org.junit.Assert.*
 import org.junit.After
@@ -20,7 +20,7 @@ class ArchetypeWalkthroughTest {
 
    private static String PS = File.separator
    private static String path = "."+ PS +"resources"+ PS +"archetypes"
-   
+
    /**
     * @throws java.lang.Exception
     */
@@ -60,42 +60,42 @@ class ArchetypeWalkthroughTest {
    {
       def loader = ArchetypeManager.getInstance(path)
       loader.loadAll()
-      
+
       def walk = new ArchetypeWalkthrough()
-      
+
       /* find constraint references acNNNN en DvCodedText */
-      
+
       def arch_constraint_codes_paths = [
       "/data[at0001]/events[at0002]/data[at0003]/items[at0004]/value/defining_code": "ac0001",
       "/data[at0001]/events[at0002]/data[at0003]/items[at0005]/value/defining_code": "ac0002",
       "/data[at0001]/events[at0002]/data[at0003]/items[at0009]/value/defining_code": "ac0003"
       ]
 
-      
+
       walk.observeCR { node, parchetype, walkt, parent -> // parent no deberia venir de la recorrida porque lo arregle en el parser... actualizar los jars
 
          /*
-         println "ConstraintRef: "+ parchetype.archetypeId.value + node.path() +" "+ 
+         println "ConstraintRef: "+ parchetype.archetypeId.value + node.path() +" "+
                           node.rmTypeName +" "+ node.nodeID +" "+ node.reference
          */
-         
+
          assert parchetype.archetypeId.value == 'openEHR-EHR-OBSERVATION.test_servicios_terminologicos.v1'
          assert node.rmTypeName == "CODE_PHRASE"
          assert arch_constraint_codes_paths.keySet().contains(node.path()) // path that contains the constraint ref
          assert arch_constraint_codes_paths[node.path()] == node.reference // acNNNN
-         
+
          // Donde pongo el mapeo entre path del nodo y el codigo de restriccion
          if (!walkt.memory['path_ccode']) walkt.memory['path_ccode'] = [:]
-         
+
          String fullPath = parchetype.archetypeId.value + node.path()
          walkt.memory['path_ccode'][fullPath] = node.reference
       }
-      
-      
-      
+
+
+
       /*
       arquetipo:
-      
+
       	constraint_bindings = <
             ["ICD10_1998"] = <
                items = <
@@ -109,7 +109,7 @@ class ArchetypeWalkthroughTest {
       walk.observeOntBind { bindings, parchetype, walkt, attr ->
 
          //println "bindings class: "+ bindings.class //  List<OntologyBinding>
-      
+
          // Verifica si hay codigos de restricciones en constraint_bindings
          // Si hay, quiero la URL que define el codigo que se usa para identificar
          // el servicio terminologico donde se an a buscar los terminos
@@ -118,34 +118,34 @@ class ArchetypeWalkthroughTest {
          {
             // Donde pongo los mapeos entre codigo de restriccion y query
             if (!walkt.memory['ccode_query']) walkt.memory['ccode_query'] = [:]
-         
+
             assert bindings.size() == 1
-            
+
             def ontologyBinding = bindings[0]
-            
+
             assert ontologyBinding.terminology == "ICD10_1998"
-         
-         
+
+
             // List<OntologyBindingItem>
             assert ontologyBinding.bindingList.size() == 1
-         
+
             def queryBindingItem = ontologyBinding.bindingList[0]
-            
+
             assert queryBindingItem.code == "ac0002"
             assert queryBindingItem.query.url == "terminology:ICD10_1998?subset=XIX%20Trauma"
-            
-            
+
+
             bindings.each { ontobind ->
-            
+
                ontobind.bindingList.each { item ->
-               
+
                   walkt.memory['ccode_query'] << [(item.code): item.query.url]
                }
             }
-            
-            
-            
-                        
+
+
+
+
             //OntBinding: [org.openehr.am.archetype.ontology.OntologyBinding@b56559[
             //  terminology=ICD10_1998
             //  bindingList=[org.openehr.am.archetype.ontology.QueryBindingItem@17b6178[
@@ -157,18 +157,18 @@ class ArchetypeWalkthroughTest {
             //]]
          }
       }
-      
-      
+
+
       def archetype = loader.getArchetype("openEHR-EHR-OBSERVATION.test_servicios_terminologicos.v1")
       walk.init(archetype)
       walk.start()
       println "memory: "+ walk.memory
-      
+
       /*
       memory: [
         path_ccode: [
-          openEHR-EHR-OBSERVATION.test_servicios_terminologicos.v1/data[at0001]/events[at0002]/data[at0003]/items[at0004]/value/defining_code:ac0001, 
-          openEHR-EHR-OBSERVATION.test_servicios_terminologicos.v1/data[at0001]/events[at0002]/data[at0003]/items[at0005]/value/defining_code:ac0002, 
+          openEHR-EHR-OBSERVATION.test_servicios_terminologicos.v1/data[at0001]/events[at0002]/data[at0003]/items[at0004]/value/defining_code:ac0001,
+          openEHR-EHR-OBSERVATION.test_servicios_terminologicos.v1/data[at0001]/events[at0002]/data[at0003]/items[at0005]/value/defining_code:ac0002,
           openEHR-EHR-OBSERVATION.test_servicios_terminologicos.v1/data[at0001]/events[at0002]/data[at0003]/items[at0009]/value/defining_code:ac0003
         ],
         ccode_query: [
